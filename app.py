c0243372809fa73dbf0767431c63f6ac581ce7da
WeirdoWrench <giridheran007@gmail.com>
2026-08-08 15:48:24 +0530

    Changed git username

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://giridhera-ramanan.github.io"],
+    allow_origins=["https://giridhera-ramanan-portfolio.github.io"],
     allow_methods=["GET", "POST", "OPTIONS"],  
     allow_headers=["*"],
 )
